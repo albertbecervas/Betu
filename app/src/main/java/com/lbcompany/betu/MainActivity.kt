25e7699181b6e5dc291c398343aed5368b4858e0
albertbecervas <albertbecerrahervas@gmail.com>
2017-09-16 14:21:22 +0200

    main screen design finished

--- app/src/main/java/com/lbcompany/betu/MainActivity.kt
+++ app/src/main/java/com/lbcompany/betu/MainActivity.kt
@@ -16,46 +16,66 @@ class MainActivity : AppCompatActivity() {
 
         val list = ArrayList<Any>()
 
+        setAdapterList(list)
+
+        setMyBetsAdapter(list)
+        setPopularAdapter(list)
+        setCategoriesAdapter(list)
+
+        setViews()
+
+    }
+
+    private fun setViews() {
+        my_bets_layout.setOnClickListener {
+            changeVisibility(my_bets_recycler)
+        }
+
+        most_popular_layout.setOnClickListener {
+            changeVisibility(most_popular_recycler)
+        }
+
+        categories_layout.setOnClickListener {
+            changeVisibility(categories_recycler)
+        }
+    }
+
+    private fun changeVisibility(itemView: View) {
+        if (itemView.visibility == View.GONE) {
+            itemView.visibility = View.VISIBLE
+        } else {
+            itemView.visibility = View.GONE
+        }
+    }
+
+    private fun setMyBetsAdapter(list: ArrayList<Any>) {
+        my_bets_recycler.visibility = View.VISIBLE
+        val mAdapter = MyBetsAdapter(this@MainActivity, list)
+        val layoutManager = LinearLayoutManager(this@MainActivity)
+        my_bets_recycler.layoutManager = layoutManager
+        my_bets_recycler.adapter = mAdapter
+    }
+
+    private fun setPopularAdapter(list: ArrayList<Any>) {
+        most_popular_recycler.visibility = View.VISIBLE
+        val mAdapter = MyBetsAdapter(this@MainActivity, list)
+        val layoutManager = LinearLayoutManager(this@MainActivity)
+        most_popular_recycler.layoutManager = layoutManager
+        most_popular_recycler.adapter = mAdapter
+    }
+
+    private fun setCategoriesAdapter(list: ArrayList<Any>) {
+        categories_recycler.visibility = View.VISIBLE
+        val mAdapter = MyBetsAdapter(this@MainActivity, list)
+        val layoutManager = LinearLayoutManager(this@MainActivity)
+        categories_recycler.layoutManager = layoutManager
+        categories_recycler.adapter = mAdapter
+    }
+
+    private fun setAdapterList(list: ArrayList<Any>) {
         list.add("my first bet")
         list.add("my second bet")
         list.add("my third bet")
         list.add("more bets...")
-
-        my_bets_layout.setOnClickListener {
-            if (my_bets_recycler.visibility == View.GONE) {
-                my_bets_recycler.visibility = View.VISIBLE
-                val mAdapter = MyBetsAdapter(this@MainActivity, list)
-                val layoutManager = LinearLayoutManager(this@MainActivity)
-                my_bets_recycler.layoutManager = layoutManager
-                my_bets_recycler.adapter = mAdapter
-            } else{
-                my_bets_recycler.visibility = View.GONE
-            }
-        }
-
-        most_popular_layout.setOnClickListener {
-            if (most_popular_recycler.visibility == View.GONE) {
-                most_popular_recycler.visibility = View.VISIBLE
-                val mAdapter = MyBetsAdapter(this@MainActivity, list)
-                val layoutManager = LinearLayoutManager(this@MainActivity)
-                most_popular_recycler.layoutManager = layoutManager
-                most_popular_recycler.adapter = mAdapter
-            } else{
-                most_popular_recycler.visibility = View.GONE
-            }
-        }
-
-        categories_layout.setOnClickListener {
-            if (categories_recycler.visibility == View.GONE) {
-                categories_recycler.visibility = View.VISIBLE
-                val mAdapter = MyBetsAdapter(this@MainActivity, list)
-                val layoutManager = LinearLayoutManager(this@MainActivity)
-                categories_recycler.layoutManager = layoutManager
-                categories_recycler.adapter = mAdapter
-            } else{
-                categories_recycler
-                        .visibility = View.GONE
-            }        }
-
     }
 }
